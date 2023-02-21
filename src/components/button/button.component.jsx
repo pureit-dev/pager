7c8cad8c5f2b64034f94441052713b4773a93689
Keith Gibson <keithwgibson74@gmail.com>
2023-02-21 10:05:53 +0000

    Finished styling components and added sound to buttons

--- src/components/button/button.component.jsx
+++ src/components/button/button.component.jsx
@@ -1,22 +1,31 @@
-import {useContext} from 'react';
+import { useContext } from "react"
 import { DisplayContext } from "../../contexts/display.context"
+import "./button.styles.css"
+import pager from "../../assets/pager.wav"
 
-
-const Button = ({text}) => {
+const Button = ({ text }) => {
 	const { setDisplay, showNums, setShowNums } = useContext(DisplayContext)
+	const audio = new Audio(pager)
 
-  const handleClick = (event) => {
-    if(event.target.id.toUpperCase() === 'RESET') {
-      setDisplay([])
-      setShowNums(false)
-    } else if(event.target.id.toUpperCase() === 'SEND'){
-      setShowNums(true)
-    }
-  }
+	const handleClick = (event) => {
+		if (event.target.id.toUpperCase() === "RESET") {
+			setDisplay([])
+			setShowNums(false)
+		} else if (event.target.id.toUpperCase() === "SEND") {
+			setShowNums(true)
+			audio.play()
+		}
+	}
 
-  return (
-    <button id={text} className={`${text}-button`} onClick={handleClick}>{text.toUpperCase()}</button>
-  )
+	return (
+		<button
+			id={text}
+			className={`${text.toLowerCase()}-button`}
+			onClick={handleClick}
+		>
+			{text.toUpperCase()}
+		</button>
+	)
 }
 
-export default Button;
+export default Button
--- src/components/number/number.component.jsx
+++ src/components/number/number.component.jsx
@@ -1,26 +1,41 @@
 import { useContext } from "react"
 import { DisplayContext } from "../../contexts/display.context"
 import "./number.styles.css"
+import wav0 from "../../assets/0.wav"
+import wav1 from "../../assets/1.wav"
+import wav2 from "../../assets/2.wav"
+import wav3 from "../../assets/3.wav"
+import wav4 from "../../assets/4.wav"
+import wav5 from "../../assets/5.wav"
+import wav6 from "../../assets/6.wav"
+import wav7 from "../../assets/7.wav"
+import wav8 from "../../assets/8.wav"
+import wav9 from "../../assets/9.wav"
+
+const wavFiles = [wav0, wav1, wav2, wav3, wav4, wav5, wav6, wav7, wav8, wav9]
 
 const Num = ({ num }) => {
-	const { display, setDisplay } = useContext(DisplayContext)
+	const { display, setDisplay, showNums } = useContext(DisplayContext)
 
 	const handleClick = (event) => {
-		const nextNumber = [...display, event.target.id]
-		setDisplay(nextNumber)
+		if (display.length < 11) {
+			const nextNumber = [...display, event.target.id]
+			setDisplay(nextNumber)
+			const audio = new Audio(wavFiles[event.target.id])
+			audio.play()
+		}
 	}
 
 	return (
-		
-			<button
-				key={num}
-				id={num}
-				className="num-button"
-				onClick={handleClick}
-			>
-				{num}
-			</button>
-		
+		<button
+			key={num}
+			id={num}
+			className="num-button"
+			onClick={handleClick}
+			disabled={showNums}
+		>
+			{num}
+		</button>
 	)
 }
 
